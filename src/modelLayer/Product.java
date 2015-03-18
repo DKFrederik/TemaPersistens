964d09278125a1b44983dfda5d319b2268603d76
nichlaslp <nichlaslp@gmail.com>
2015-03-18 11:56:29 +0100

    Product class 1st version

---
+++ src/modelLayer/Product.java
@@ -0,0 +1,124 @@
+package modelLayer;
+ /** 
+  * Product class.
+  * Details a product. 
+  * @author Nichlas, Peter, Claus, Frederik.
+  * @version 0.1
+  *
+  */
+public class Product {
+	
+	private int id;
+	private int minStock;
+	private String name;
+	private String originCountry;
+	private double purchasePrice;
+	private double rentPrice;
+	private double salesPrice;
+	private Supplier supplier;
+	
+	/**
+	 * @param id product id.
+	 * @param name product name.
+	 * @param supplier name of the supplier.
+	 * @param purchasePrice the price of the product.
+	 * @param salesPrice what the product retails for.
+	 * @param rentPrice price of renting.
+	 * @param originCountry Country where made.
+	 * @param minStock the minimumstock of the product
+	 */
+	public Product(int id, int minStock, String name, String originCountry,
+			double purchasePrice, double rentPrice, double salesPrice,
+			Supplier supplier) 
+	{
+		this.id = id;
+		this.minStock = minStock;
+		this.name = name;
+		this.originCountry = originCountry;
+		this.purchasePrice = purchasePrice;
+		this.rentPrice = rentPrice;
+		this.salesPrice = salesPrice;
+		this.supplier = supplier;
+	}
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+	public int getMinStock() 
+	{
+		return minStock;
+	}
+	
+	public void setMinStock(int minStock) 
+	{
+		this.minStock = minStock;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	public String getOriginCountry() 
+	{
+		return originCountry;
+	}
+	
+	public void setOriginCountry(String originCountry) 
+	{
+		this.originCountry = originCountry;
+	}
+	
+	public double getPurchasePrice() 
+	{
+		return purchasePrice;
+	}
+	
+	public void setPurchasePrice(double purchasePrice) 
+	{
+		this.purchasePrice = purchasePrice;
+	}
+	
+	public double getRentPrice() 
+	{
+		return rentPrice;
+	}
+	
+	public void setRentPrice(double rentPrice) 
+	{
+		this.rentPrice = rentPrice;
+	}
+	
+	public double getSalesPrice() 
+	{
+		return salesPrice;
+	}
+	
+	public void setSalesPrice(double salesPrice) 
+	{
+		this.salesPrice = salesPrice;
+	}
+	
+	public Supplier getSupplier() 
+	{
+		return supplier;
+	}
+	
+	public void setSupplier(Supplier supplier) 
+	{
+		this.supplier = supplier;
+	}
+
+}
